d7d54dbef7394b409d8638076a1230ff72ca15a2
Ahmed Elsenousi <aelsenou@amazon.com>
2024-08-01 16:53:52 +0100

    validaton

--- cdk_workshop/cdk_workshop_stack.py
+++ cdk_workshop/cdk_workshop_stack.py
@@ -1,21 +1,28 @@
 from constructs import Construct
 from aws_cdk import (
     Stack,
+    CfnOutput,
     aws_lambda as _lambda,
     aws_apigateway as apigw,
 )
-
 from cdk_dynamo_table_view import TableViewer
 from .hitcounter import HitCounter
 
-
 class CdkWorkshopStack(Stack):
+
+    @property
+    def hc_endpoint(self):
+        return self._hc_endpoint
+
+    @property
+    def hc_viewer_url(self):
+        return self._hc_viewer_url
 
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
         # Defines an AWS Lambda resource
-        hello = _lambda.Function(
+        my_lambda = _lambda.Function(
             self, 'HelloHandler',
             runtime=_lambda.Runtime.PYTHON_3_8,
             code=_lambda.Code.from_asset('lambda'),
@@ -24,16 +31,26 @@ class CdkWorkshopStack(Stack):
 
         hello_with_counter = HitCounter(
             self, 'HelloHitCounter',
-            downstream=hello,
+            downstream=my_lambda
         )
 
-        apigw.LambdaRestApi(
+        gateway = apigw.LambdaRestApi(
             self, 'Endpoint',
-            handler=hello_with_counter._handler,
+            handler=hello_with_counter._handler
         )
 
-        TableViewer(
+        tv = TableViewer(
             self, 'ViewHitCounter',
             title='Hello Hits',
-            table=hello_with_counter.table,
+            table=hello_with_counter.table
+        )
+
+        self._hc_endpoint = CfnOutput(
+            self, 'GatewayUrl',
+            value=gateway.url
+        )
+
+        self._hc_viewer_url = CfnOutput(
+            self, 'TableViewerUrl',
+            value=tv.endpoint
         )
